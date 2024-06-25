1d2b3bf3e5e04f2b535198bb8eb24b6d7697d89d
Faizan Khan <m.faisalkhan9@gmail.com>
2024-06-25 12:43:21 +0530

    update

--- src/Views/Admin/Parts/PaymentHistory.jsx
+++ src/Views/Admin/Parts/PaymentHistory.jsx
@@ -150,6 +150,8 @@ const PaymentHistory = () => {
                 responseType: 'blob',
             });
 
+            console.log(response.data);
+
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
